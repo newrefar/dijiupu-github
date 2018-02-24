0d43cd6daf3d67c08ea4e9fa8e703b01f52ac997
layo <refar@163.com>
2018-02-24 15:28:12 +0800

    Update MD5Util.java

--- xtedu-crack/src/main/java/com/dijiupu/xteducrack/utils/MD5Util.java
+++ xtedu-crack/src/main/java/com/dijiupu/xteducrack/utils/MD5Util.java
@@ -6,7 +6,6 @@ import java.util.Random;
 import java.util.UUID;
 
 public class MD5Util {
-	
 	// 十六进制下数字到字符的映射数组
 	private final static String[] hexDigits = { "0", "1", "2", "3", "4", "5",
 			"6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
